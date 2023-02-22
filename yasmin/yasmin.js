f20ee6b6ee1b69944a5962b7e8fff238e7ada367
lightpotat0 <113602502+lightpotat0@users.noreply.github.com>
2023-02-22 16:35:47 -0300

    Update yasmin.js

--- yasmin/yasmin.js
+++ yasmin/yasmin.js
@@ -35,90 +35,23 @@ function sla() {
     if (c < m) {
         switch (c) {
             case 1:
-                texto1.innerHTML = '<h1>Olha, eu queria fazer algo legal kkk, eu sei que isso é bem nerdola</h1>'
-                nerd.style.display = 'block'
+                texto1.innerHTML = '<h1>Esse é meu pedido real de namoro, eu quero um recomeço 100% sincero</h1>'
+                buque.style.display = 'block'
                 texto1.style.fontSize = '1em'
                 break;
             case 2:
-                texto1.innerHTML = '<h1>Vamos deixar de conversa e vamos logo para o joguinho</h1>'
-                nerd.style.display = 'none'
+                texto1.innerHTML = '<h1>Então, Eu te amo muito e no momento que você chegou melhorou minha vida na hora, e eu não cuidei, mas dessa vez vou cuidar de verdade porque eu te amo muito e nunca mais vou te perder, então quer namorar comigo?</h1>'
+                pidao.style.display = 'block'
                 texto1.style.fontSize = '1em'
-                break;
-            case 3:
-                texto1.innerHTML = '<h1>Vamos Pegue essa goiaba :)</h1>'
-                goiaba.style.display = 'block'
-                botao.style.display = 'none'
-                break;
-            case 4:
-                goiaba.style.marginLeft = '20%'
-                break;
-            case 5:
-                goiaba.style.marginLeft = '50%'
-                goiaba.style.marginTop = '10%'
-                break;
-            case 6:
-                goiaba.style.marginLeft = '40%'
-                goiaba.style.marginTop = '5%'
-                break;
-            case 7:
-                texto1.innerHTML = '<h1>Você por acaso é lesada gatinha? Não tem como comer essa goiaba, ela é uma imagem XD</h1>'
-                goiaba.style.display = 'none'
-                rindo.style.display = 'block'
-                botao.style.display = 'block'
-                break;
-            case 8:
-                texto1.innerHTML = '<h1>Agora vamos ver se você não é lesada mesmo,<br> Qual desses é uma gatinha?</h1>'
-                botao.style.display = 'none'
-                rindo.style.display = 'none'
-                choose.style.display = 'block'
-                break;
-            case 9:
-                texto1.innerHTML = '<h1>Você acertou se escolheu essa</h1>'
-                pidao.style.display = "none"
-                botao.style.display = 'block'
-                yasmin.style.paddingRight = '0%'
-                break;
-            case 10:
-                texto1.innerHTML = '<h1>VAMOS UMA CANTADA AGORA</h1>'
-                love.style.display = 'block'
-                choose.style.display = 'none'
-                break;
-            case 11:
-                texto1.innerHTML = '<h1>Seu nome é Wi-Fi? Porque eu estou sentindo uma conexão aqui.</h1>'
-                oi.style.display = 'block'
-                love.style.display = 'none'
-                oi.style.padding = '10px'
-                break;
-            case 12:
-                texto1.innerHTML = '<h1>KK desculpa, só queria ser legal</h1>'
-                timido.style.display = 'block'
-                oi.style.display = 'none'
-                break;
-            case 13:
-                texto1.innerHTML = '<h1>De verdade, você é a pessoa mais especial e incrivel que conheci<br> e que amo muito</h1>'
-                break;
-            case 14:
-                texto1.innerHTML = '<h1>Por isso queria saber...<br>Você gosta de mim?</h1>'
-                botao.style.display = 'none'
-                timido.style.display = 'none'
                 choose1.style.display = 'block'
                 break;
-            case 15:
-                texto1.innerHTML = '<h1>Que bom gatinha, eu fico muito feliz :)</h1>'
-                botao.style.display = 'block'
-                choose1.style.display = 'none'
-                love.style.display = 'block'
-                break;
-            case 16:
-                texto1.innerHTML = '<h1>Mas agora estou convidando para sair comigo :) <br> responda no zap</h1>'
-                love.style.display = 'block'
-                break;
-            case 17:
-                texto1.innerHTML = '<h1>Te amo</h1>'
-                love.style.display = 'none'
+            case 3:
+                texto1.innerHTML = '<h1>Me responda no zap :)</h1>'
                 catdance.style.display = 'block'
+                buque.style.display = 'none'
                 botao.style.display = 'none'
-                break;   
+                choose1.style.display = 'none'
+                break;
         }
         c++
     }
